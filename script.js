26b3448e459f724c228502c85be7863d8b881d37
aziz089 <azizcn61@gmail.com>
2026-04-29 18:18:48 +0300

    init fonksiyonu eklendi

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 ﻿// Main Script
 'use strict';
-console.log('Merhaba Dunya');
-console.log('Proje baslatildi');
-console.log('Versiyon: 1.0.0');
+
+function init() {
+  console.log('Merhaba Dunya');
+  console.log('Proje baslatildi');
+  console.log('Versiyon: 1.0.0');
+}
+
+init();
